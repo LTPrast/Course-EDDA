0dab4f014682c67f4de85810115e6314f8d56ff4
Jamie Faber <jamiefaber@hotmail.com>
2022-02-22 18:19:07 +0100

    improved exercise 2

--- exercise2.R
+++ exercise2.R
@@ -1,4 +1,4 @@
-#2a
+#2a # leads to more rain fall <- test for greater?
 # plots
 par(mfrow=c(2,2))
 qqnorm(clouds$seeded);hist(clouds$seeded)
@@ -14,23 +14,23 @@ boxplot(clouds$seeded);boxplot(clouds$unseeded)
 # f test significant so significant differences between variances
 var.test(clouds$seeded, clouds$unseeded, alternative = "two.sided")
 # two samples t-test
-t.test(clouds$seeded,clouds$unseeded)
+t.test(clouds$seeded,clouds$unseeded,alternative = "greater")
 # Mann-Whitney test
-wilcox.test(clouds$seeded,clouds$unseeded)
+wilcox.test(clouds$seeded,clouds$unseeded,alternative = "greater")
 # Kolmogorov-Smirnov test
-ks.test(clouds$seeded,clouds$unseeded)
+ks.test(clouds$unseeded,clouds$seeded,alternative = "greater")
 # is data paired? 
 # Chose clouds so independent/not paired,also clouds aren’t identical, 
 # think twins for paired test or the same person/thing used twice.
 # Are tests applicable?
-# mann whitney and komogorov need the samples to come 
+# mann whitney and kolmogorov need the samples to come 
 # from different populations, so be independent. This assumption can be withhold.
 # T-test isnt applicable due to assumption of normality being violated. Variances
 # also significantly differ so t.test cant be used with var.equal = true. 
 # Comment on findings:
 # Non parametric tests more significant due to outliers for which these 
 # tests aren't as sensitive for, since mann whitney is a rank test (median)
-# and komogorov checks the differences of the verticality of the histograms,
+# and kolmogorov checks the differences of the verticality of the histograms,
 # since outliers, rare, verticality of histogram for outlier small.
 
 
@@ -54,11 +54,11 @@ boxplot(sqrtunseeded)
 # f test significant if significant differences between variances
 var.test(sqrtseeded, sqrtunseeded, alternative = "two.sided")
 # two samples t-test: 
-t.test(sqrtseeded,sqrtunseeded)
+t.test(sqrtseeded,sqrtunseeded,alternative = "greater")
 # Mann-Whitney test
-wilcox.test(sqrtseeded,sqrtunseeded)
+wilcox.test(sqrtseeded,sqrtunseeded,alternative = "greater")
 # Kolmogorov-Smirnov test
-ks.test(sqrtseeded,sqrtunseeded)
+ks.test(sqrtunseeded,sqrtseeded,alternative = "greater")
 
 # still outliers while less large, t-test assumptions still violated, 
 # transformation doesnt affect ranked tests since ranks is still the same
@@ -83,11 +83,11 @@ boxplot(sqrtsqrtunseeded)
 # f test significant so significant differences between variances
 var.test(sqrtsqrtseeded, sqrtsqrtunseeded, alternative = "two.sided")
 # two samples t-test:
-t.test(sqrtsqrtseeded,sqrtsqrtunseeded, var.equal = TRUE)
+t.test(sqrtsqrtseeded,sqrtsqrtunseeded, var.equal = TRUE,alternative = "greater")
 # Mann-Whitney test
-wilcox.test(sqrtsqrtseeded,sqrtsqrtunseeded)
+wilcox.test(sqrtsqrtseeded,sqrtsqrtunseeded,alternative = "greater")
 # Kolmogorov-Smirnov test
-ks.test(sqrtsqrtseeded,sqrtsqrtunseeded)
+ks.test(sqrtsqrtunseeded,sqrtsqrtseeded,alternative = "greater")
 # are tests now applicable with transformations
 # Mann whithey and kolmogorov were applicable the whole time, however now
 # since only 1 outlier is left, t-test assumptions not violated. 
@@ -95,51 +95,52 @@ ks.test(sqrtsqrtseeded,sqrtsqrtunseeded)
 # significant now assumptions are not violated? maybe due to outlier still having
 # an influence)
 
-#2c This code copied from lecture ppt however isn't correct
-c1=clouds[,1] #   seeded
-c2=clouds[,2] # unseeded
-T1=mean(c1); T2=mean(c2)
-T1
-T2
-
-B=1000
-Tstar=numeric(B)
-for(i in 1:B) {
-  Xstar=sample(c1,replace=TRUE)
-  Tstar[i]=mean(Xstar) }
-Tstar25=quantile(Tstar,0.025)
-Tstar975=quantile(Tstar,0.975)
-sum(Tstar<Tstar25)
-c(2*T1-Tstar975,2*T1-Tstar25)
+#2c
+c1 <- clouds$seeded
+lambda_est <- 1/mean(c1)
+n <- length(clouds$seeded)
+sd <- 1/lambda_est
+# CI
+z <- qnorm(1-0.025)
+CI_L <- (-z+sqrt(n))/(mean(c1)*sqrt(n));CI_L
+CI_R <- (z+sqrt(n))/(mean(c1)*sqrt(n));CI_R
 
 # bootstrap test
-hist(Tstar,prob=T)
-hist(Tstar,prob=T,ylim=c(0,0.7))
-x=seq(0,max(Tstar),length=1000)
-lines(x,dexp(x),type="l",col="blue",lwd=2)
-t=max(Tstar)
+t=median(c1)
 t
 B=1000
 tstar=numeric(B)
-n=length(Tstar)
+n=length(c1)
 for (i in 1:B){
-  xstar=rexp(n,1)
-  tstar[i]=max(xstar)}
+  xstar=rexp(n,lambda_est)
+  tstar[i]=median(xstar)}
 hist(tstar,prob=T)
 pl=sum(tstar<t)/B; pr=sum(tstar>t)/B; p=2*min(pl,pr)
 pl;pr;p
 
+#install.packages("pracma")
+#library("pracma")
+cdf_x <- linspace(min(c1), max(c1), 101)
+sorted_c1 <- sort(c1);
 # Kolmogorov-Smirnov test
-ks.test(c1,c2)
+par(mfrow=c(1,1))
+c1_quantiles  <- linspace(0,1,length(c1))
+# plots actual values of clouds$seeded
+plot(sorted_c1,c1_quantiles)
+# line of expected values is exp dist with rate est lambda
+lines(cdf_x,pexp(cdf_x,lambda_est),type="l",col="blue",lwd=2)
+ks.test(clouds$seeded,"pexp", lambda_est)
 
 # 2d
 #is median precipitation for seeded clouds less than 300?
+hist(clouds$seeded)
+# not symmetric so cant use wilcox
 x <- sum(clouds$seeded<300)
 n <- length(clouds$seeded)
 binom.test(x,n,p=0.5,alt='less')
 # not significant (P = 0.9622) so h0 not rejected: which is medians are the same
 
-# is fraction seeded clouds with precipertation less than 30 under 25%? 
+# is fraction seeded clouds with precipitation less than 30 under 25%? 
 x <- sum(clouds$seeded<30)
 binom.test(x,n,p=0.25,alt='less')
 # not significant (P=0.08019) h0 not rejected: which is medians are the same
